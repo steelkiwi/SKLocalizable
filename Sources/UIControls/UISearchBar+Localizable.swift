89a1d03e268b4ab5d4752f6994b3d43a5bfa2396
Artem Zinuk <az@letyshops.com>
2019-02-25 14:31:24 +0200

    add UISearchBar localize

---
+++ Sources/UIControls/UISearchBar+Localizable.swift
@@ -0,0 +1,22 @@
+//
+//  UISearchBar+Localizable.swift
+//  SKLocalizable
+//
+//  Created by Artem Zinuk on 25.02.19.
+//
+
+import UIKit
+
+extension UISearchBar {
+    
+    // localizationKey overrided just for IBInspectable
+    @IBInspectable
+    open override var localizationKey: String? {
+        get { return super.localizationKey }
+        set { super.localizationKey = newValue }
+    }
+    
+    open override func localize() {
+        self.placeholder = localizationKey?.localized(tableName: self.localizationFile)
+    }
+}
